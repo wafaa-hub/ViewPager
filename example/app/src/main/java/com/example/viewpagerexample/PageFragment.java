c5ccfb721a73a31f8a7fb97c766f5490636809d1
wafaa ghonaimat <wafaa@tickerchart.com>
2020-07-09 14:08:18 +0300

    AN-1323 re-code by another way

--- example/app/src/main/java/com/example/viewpagerexample/PageFragment.java
+++ example/app/src/main/java/com/example/viewpagerexample/PageFragment.java
@@ -4,19 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class PageFragment extends Fragment {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate (savedInstanceState);
+    public static final String FRAGMENT_TAG = "MainFragment";
+
+    public static PageFragment newInstance(String text) {
+        return newInstance(text, FRAGMENT_TAG);
+    }
+
+    public static PageFragment newInstance(String text, String tag) {
+        PageFragment fragment = new PageFragment();
+        Bundle args = new Bundle();
+        args.putString("text", text);
+        args.putString(MainActivity.FRAGMENT_TAG_ARG, tag + "_" + fragment.hashCode());
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate (R.layout.page, container, false);
-        return view;
+        View rootView = inflater.inflate(R.layout.page, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.textView);
+        textView.setText(getArguments().getString("text"));
+        return rootView;
     }
+
 }
