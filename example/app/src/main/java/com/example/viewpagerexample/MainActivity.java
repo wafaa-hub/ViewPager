d1cc7ee2faed47d06d39d960a68f67ee88bf384d
wafaa ghonaimat <wafaa@tickerchart.com>
2020-07-09 14:20:57 +0300

    Revert "AN-1323 re-code by another way"

--- example/app/src/main/java/com/example/viewpagerexample/MainActivity.java
+++ example/app/src/main/java/com/example/viewpagerexample/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.viewpagerexample;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.PagerTabStrip;
 import androidx.viewpager.widget.ViewPager;
@@ -16,6 +12,7 @@ import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.ClickableSpan;
 import android.text.style.ImageSpan;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,12 +20,8 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String FRAGMENT_TAG_ARG = "tag";
 
     CustomPagerAdapter adapterViewPager;
     ViewPager vpPager;
@@ -36,11 +29,9 @@ public class MainActivity extends AppCompatActivity {
     Button deleteView;
     RelativeLayout view;
     PagerTabStrip pagerTabStrip;
-    Drawable removePage;
+    Drawable removePage ;
     ClickableSpan clickableSpan;
     ImageSpan span;
-    int count = 1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
@@ -50,17 +41,17 @@ public class MainActivity extends AppCompatActivity {
         addView = findViewById (R.id.add_view);
         deleteView = findViewById (R.id.delete_view);
         pagerTabStrip = findViewById (R.id.pager_header);
-        adapterViewPager = new CustomPagerAdapter (getSupportFragmentManager ( ));
-        adapterViewPager.addPage (PageFragment.newInstance ("Page " + count));
-        adapterViewPager.notifyDataSetChanged ( );
+        adapterViewPager = new CustomPagerAdapter ( );
         vpPager.setAdapter (adapterViewPager);
+        LayoutInflater inflater = getLayoutInflater ( );
+
+        view = (RelativeLayout) inflater.inflate (R.layout.page, null);
 
         pagerTabStrip.setOnTouchListener (new View.OnTouchListener ( ) {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 clickableSpan.onClick (view);
                 return false;
-
             }
         });
 
@@ -68,177 +59,105 @@ public class MainActivity extends AppCompatActivity {
         addView.setOnClickListener (new View.OnClickListener ( ) {
             @Override
             public void onClick(View v) {
-                if (adapterViewPager.getCount ( ) == 0)
-                    count = 1;
-                else {
-                    count++;
-                }
-                adapterViewPager.addPage (PageFragment.newInstance ("Page " + count));
+                int pageIndex = adapterViewPager.addView (view);
+                vpPager.setCurrentItem (pageIndex, true);
             }
         });
 
         deleteView.setOnClickListener (new View.OnClickListener ( ) {
             @Override
             public void onClick(View v) {
-                if (adapterViewPager.getCount ( ) > 0)
-                    adapterViewPager.removePage (vpPager.getCurrentItem ( ));
+                if (adapterViewPager.getCount ( ) > 0) {
+                    int pageIndex = adapterViewPager.removeView (vpPager, view);
+                    if (pageIndex == adapterViewPager.getCount ( ))
+                        pageIndex--;
+                    vpPager.setCurrentItem (pageIndex);
+                }
             }
         });
+
         clickableSpan = new ClickableSpan ( ) {
             @Override
+
             public void onClick(@NonNull View widget) {
-                if (adapterViewPager.getCount ( ) > 0)
-                    adapterViewPager.removePage (vpPager.getCurrentItem ( ));
+                if (adapterViewPager.getCount ( ) > 0) {
+                    int pageIndex = adapterViewPager.removeView (vpPager, view);
+                    if (pageIndex == adapterViewPager.getCount ( ))
+                        pageIndex--;
+                    vpPager.setCurrentItem (pageIndex);
+                }
             }
         };
     }
 
     public class CustomPagerAdapter extends PagerAdapter {
 
-        private List<Fragment> pages = new ArrayList<> ( );
-        private Map<Fragment, Integer> fragmentsPosition = new HashMap<> ( );
-
-        private Fragment currentPrimaryItem;
-        private FragmentManager fragmentManager;
-        private FragmentTransaction currentTransaction;
-
-
-        public CustomPagerAdapter(FragmentManager fragmentManager) {
-            this.fragmentManager = fragmentManager;
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            if (currentTransaction == null) {
-                currentTransaction = fragmentManager.beginTransaction ( );
-            }
-
-            Fragment pageFragment = pages.get (position);
-            String tag = pageFragment.getArguments ( ).getString (MainActivity.FRAGMENT_TAG_ARG);
-            Fragment fragment = fragmentManager.findFragmentByTag (tag);
-
-            if (fragment != null) {
-                if (fragment.getId ( ) == container.getId ( )) {
-                    currentTransaction.attach (fragment);
-                } else {
-                    fragmentManager.beginTransaction ( ).remove (fragment).commit ( );
-                    fragmentManager.executePendingTransactions ( );
-                    currentTransaction.add (container.getId ( ), fragment, tag);
-                }
-            } else {
-                fragment = pageFragment;
-                currentTransaction.add (container.getId ( ), fragment, tag);
-            }
-
-            if (fragment != currentPrimaryItem) {
-                fragment.setMenuVisibility (false);
-                fragment.setUserVisibleHint (false);
-            }
-
-            return fragment;
-        }
+        private ArrayList<View> views = new ArrayList<View> ( );
 
         @Override
         public int getCount() {
-            return pages.size ( );
+            return views.size ( );
         }
 
         @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            if (currentTransaction == null) {
-                currentTransaction = fragmentManager.beginTransaction ( );
-            }
+        public CharSequence getPageTitle(final int position) {
 
-            currentTransaction.detach ((Fragment) object);
-        }
-
-        @Override
-        public void setPrimaryItem(ViewGroup container, int position, Object object) {
-            Fragment fragment = (Fragment) object;
-
-            if (fragment != currentPrimaryItem) {
-                if (currentPrimaryItem != null) {
-                    currentPrimaryItem.setMenuVisibility (false);
-                    currentPrimaryItem.setUserVisibleHint (false);
-                }
-
-                if (fragment != null) {
-                    fragment.setMenuVisibility (true);
-                    fragment.setUserVisibleHint (true);
-                }
-
-                currentPrimaryItem = fragment;
-            }
-        }
-
-        @Override
-        public void finishUpdate(ViewGroup container) {
-            if (currentTransaction != null) {
-                currentTransaction.commitAllowingStateLoss ( );
-                currentTransaction = null;
-                fragmentManager.executePendingTransactions ( );
-            }
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return ((Fragment) object).getView ( ) == view;
-        }
-
-        @Override
-        public int getItemPosition(Object o) {
-            Integer result = fragmentsPosition.get (o);
-
-            if (result == null) {
-                return PagerAdapter.POSITION_UNCHANGED;
-            }
-
-            return result;
-        }
-
-
-        public void addPage(Fragment fragment) {
-            fragmentsPosition.clear ( );
-            pages.add (fragment);
-            adapterViewPager.notifyDataSetChanged ( );
-        }
-
-        public void removePage(int position) {
-            fragmentsPosition.clear ( );
-            Fragment pageFragment = pages.get (position);
-            String tag = pageFragment.getArguments ( ).getString (MainActivity.FRAGMENT_TAG_ARG);
-
-            Fragment fragment = fragmentManager.findFragmentByTag (tag);
-
-            if (fragment != null) {
-                fragmentsPosition.put (fragment, PagerAdapter.POSITION_NONE);
-            }
-
-            for (int i = position + 1; i < pages.size ( ); i++) {
-                pageFragment = pages.get (i);
-                tag = pageFragment.getArguments ( ).getString (MainActivity.FRAGMENT_TAG_ARG);
-                fragment = fragmentManager.findFragmentByTag (tag);
-
-                if (fragment != null) {
-                    fragmentsPosition.put (fragment, i - 1);
-                }
-            }
-
-            pages.remove (position);
-            adapterViewPager.notifyDataSetChanged ( );
-
-        }
-
-        @Nullable
-        @Override
-        public CharSequence getPageTitle(int position) {
             SpannableStringBuilder sb = new SpannableStringBuilder (" Page #" + (position + 1));
             removePage = getApplicationContext ( ).getDrawable (R.drawable.remove_page);
             removePage.setBounds (0, 15, 60, 50);
             span = new ImageSpan (removePage, ImageSpan.ALIGN_BASELINE);
             sb.setSpan (span, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-
             return sb;
         }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object view) {
+            container.removeView (views.get (position));
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return view == object;
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+            return PagerAdapter.POSITION_NONE;
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            View v = views.get (position);
+            if (v.getParent ( ) != null) {
+                ((ViewGroup) v.getParent ( )).removeView (v);
+            }
+            container.addView (v);
+            return v;
+        }
+
+        public int addView(View v, int position) {
+            views.add (position, v);
+            adapterViewPager.notifyDataSetChanged ( );
+            return position;
+        }
+
+        public int addView(View v) {
+            return addView (v, views.size ( ));
+        }
+
+        public int removeView(ViewPager pager, View v) {
+            return removeView (pager, views.indexOf (v));
+        }
+
+        public int removeView(ViewPager pager, int position) {
+            pager.setAdapter (null);
+            views.remove (position);
+            pager.setAdapter (this);
+            adapterViewPager.notifyDataSetChanged ( );
+            return position;
+        }
     }
+
+
 }
